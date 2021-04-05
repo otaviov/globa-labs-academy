c8a45b808355af0c2a632f480491a856cf6b190e
otaviov <amandavv32@gmail.com>
2021-04-05 19:37:39 -0300

    inciando o spring web mvc

---
+++ src/main/java/com/globallabs/springwebmdc/SpringWebMdcApplication.java
@@ -0,0 +1,13 @@
+package com.globallabs.springwebmdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMdcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/globallabs/springwebmdc/SpringWebMdcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globallabs.springwebmdc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebMdcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
