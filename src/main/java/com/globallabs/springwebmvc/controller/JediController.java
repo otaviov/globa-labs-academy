9cb603ee0b1bef56a7fae499f1dce6d696d229bf
otaviov <amandavv32@gmail.com>
2021-04-06 22:55:23 -0300

    revendo bugs

--- src/main/java/com/globallabs/springwebmvc/controller/JediController.java
+++ src/main/java/com/globallabs/springwebmvc/controller/JediController.java
@@ -1,9 +1,6 @@
 package com.globallabs.springwebmvc.controller;
-
 import java.util.List;
-
 import com.globallabs.springwebmvc.model.Jedi;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
