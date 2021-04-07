737366af7acdee5154f8b10403271734a976d8ad
otaviov <amandavv32@gmail.com>
2021-04-06 23:09:47 -0300

    Update JediController.java

--- src/main/java/com/globallabs/springwebmvc/controller/JediController.java
+++ src/main/java/com/globallabs/springwebmvc/controller/JediController.java
@@ -1,6 +1,9 @@
 package com.globallabs.springwebmvc.controller;
+
 import java.util.List;
+
 import com.globallabs.springwebmvc.model.Jedi;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
